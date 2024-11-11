369498d8fef3c56eac7ef2414b6af2d0153efc17
Liudmila-Shapovalova <shapovalova@goleks.com>
2024-11-11 20:45:22 +0200

    [feature/TWP-1337]:Learn Variables

---
+++ src/main/java/lv/acodemy/MyVariables.java
@@ -0,0 +1,50 @@
+package lv.acodemy;
+
+public class MyVariables {
+    public static void main(String[] args) {
+
+        // This is my comment
+
+        /*
+        This is first line
+        This is second line
+        This is third line
+         */
+        // int (integer) - whole number / natural number
+        int age = 30;
+        int currentYear = 2024;
+
+        // currentYear = camel case (lower camel case) - good practice
+        // current_year = snake case - never do like this!!!
+        // current-year = kebab case - never do like this!!!
+
+        int inventoryItemCount = 100;
+
+        System.out.println(age);
+        System.out.println(currentYear);
+        System.out.println(inventoryItemCount);
+
+        // double (fractional numbers)
+        double temperature = 36.6;
+        double price = 19.99;
+        double distance = 38400.0;
+
+        System.out.println(temperature);
+        System.out.println(price);
+
+        // char (symbols)
+
+        char grade = 'A';
+        char myInitial = 'N';
+        char currencySymbol = '$';
+
+        // Print them all;
+
+        System.out.println(grade);
+        System.out.println(myInitial);
+        System.out.println(currencySymbol);
+
+
+
+    };
+}
